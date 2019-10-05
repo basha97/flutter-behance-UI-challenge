c619798e266f093dec8ef8f0269a91272e7507ab
basha97 <bashaadil2727@gmail.com>
2019-10-05 13:14:08 +0530

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,117 @@
 import 'package:flutter/material.dart';
+import './fluttercolor.dart';
+import './login_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-    // This widget is the root of your application.
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData(
-                // This is the theme of your application.
-                //
-                // Try running your application with "flutter run". You'll see the
-                // application has a blue toolbar. Then, without quitting the app, try
-                // changing the primarySwatch below to Colors.green and then invoke
-                // "hot reload" (press "r" in the console where you ran "flutter run",
-                // or simply save your changes to "hot reload" in a Flutter IDE).
-                // Notice that the counter didn't reset back to zero; the application
-                // is not restarted.
-                primarySwatch: Colors.blue,
-            ),
-            home: MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-    }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {'/': (ctx) => MyHomePage(), '/login': (ctx) => LoginPage()},
+      title: 'Behance Challenge',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+    );
+  }
 }
 
-class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
-
-    // This widget is the home page of your application. It is stateful, meaning
-    // that it has a State object (defined below) that contains fields that affect
-    // how it looks.
-
-    // This class is the configuration for the state. It holds the values (in this
-    // case the title) provided by the parent (in this case the App widget) and
-    // used by the build method of the State. Fields in a Widget subclass are
-    // always marked "final".
-
-    final String title;
-
-    @override
-    _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    int _counter = 0;
-
-    void _incrementCounter() {
-        setState(() {
-            // This call to setState tells the Flutter framework that something has
-            // changed in this State, which causes it to rerun the build method below
-            // so that the display can reflect the updated values. If we changed
-            // _counter without calling setState(), then the build method would not be
-            // called again, and so nothing would appear to happen.
-            _counter++;
-        });
-    }
-
-    @override
-    Widget build(BuildContext context) {
-        // This method is rerun every time setState is called, for instance as done
-        // by the _incrementCounter method above.
-        //
-        // The Flutter framework has been optimized to make rerunning build methods
-        // fast, so that you can just rebuild anything that needs updating rather
-        // than having to individually change instances of widgets.
-        return Scaffold(
-            appBar: AppBar(
-                // Here we take the value from the MyHomePage object that was created by
-                // the App.build method, and use it to set our appbar title.
-                title: Text(widget.title),
+class MyHomePage extends StatelessWidget {
+  bool _isEnabled = true;
+  Color mainColor = Color(0xffb74093);
+  Color _sky_blue = FlutterColor("#0060FF");
+  Color _grass_green = FlutterColor("#aee675");
+  Color _solid_black = FlutterColor("#06213d");
+  Color _background_color = FlutterColor("#f2f7ff");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _background_color,
+      body: SafeArea(
+        top: _isEnabled,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 75.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image.network(
+                      'https://ezfleets.com/backend/images/logo.png',
+                      fit: BoxFit.cover,
+                      width: 65,
+                      height: 75,
+                    ),
+                    width: MediaQuery.of(context).size.width * 0.65,
+                  )
+                ],
+              ),
             ),
-            body: Center(
-                // Center is a layout widget. It takes a single child and positions it
-                // in the middle of the parent.
-                child: Column(
-                    // Column is also a layout widget. It takes a list of children and
-                    // arranges them vertically. By default, it sizes itself to fit its
-                    // children horizontally, and tries to be as tall as its parent.
-                    //
-                    // Invoke "debug painting" (press "p" in the console, choose the
-                    // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                    // to see the wireframe for each widget.
-                    //
-                    // Column has various properties to control how it sizes itself and
-                    // how it positions its children. Here we use mainAxisAlignment to
-                    // center the children vertically; the main axis here is the vertical
-                    // axis because Columns are vertical (the cross axis would be
-                    // horizontal).
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Text(
-                            'You have pushed the button this many times:',
-                        ),
-                        Text(
-                            '$_counter',
-                            style: Theme.of(context).textTheme.display1,
-                        ),
-                    ],
-                ),
+            SizedBox(
+              height: 10,
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
-        );
-    }
+            Padding(
+              padding: EdgeInsets.only(top: 35.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: RaisedButton(
+                      onPressed: () {},
+                      elevation: 1,
+                      color: _sky_blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0)),
+                      child: Padding(
+                        padding: EdgeInsets.all(18.0),
+                        child: Text(
+                          'Create an account',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Arimo-Bold'),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 55.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: GestureDetector(
+                      onTap: (){
+                        Navigator.pushNamed(context, '/login');
+                      },
+                        child: Text(
+                      'LOG IN',
+                      style: TextStyle(
+                          color: _grass_green,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
